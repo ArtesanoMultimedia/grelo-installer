dd0014ef747905bfbfa7003aafe95006055aeba8
ArtesanoMultimedia <artesanomultimedia@gmail.com>
2020-06-02 01:09:26 +0200

    Commit inicial

---
+++ src/NewCommand.php
@@ -0,0 +1,226 @@
+<?php
+
+
+namespace Grelo\Installer\Console;
+
+use GuzzleHttp\Client;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
+use ZipArchive;
+
+class NewCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('new')
+            ->setDescription('Crea una nueva aplicación basada en el Grelo Framework')
+            ->addArgument('name', InputArgument::OPTIONAL)
+            ->addOption('dev', null, InputOption::VALUE_NONE, 'Crea un proyecto nuevo desde la rama en desarrollo')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Fuerza la instalación incluso si el directorio ya existe');;
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (version_compare(PHP_VERSION, '7.3.0', '<')) {
+            throw new RuntimeException('El instalador de Grelo Framework requiere al menos la versión 7.3.0 de PHP.');
+        }
+
+        if (!extension_loaded('zip')) {
+            throw new RuntimeException('No está instalada la extensión Zip PHP. Instálela e inténtelo de nuevo.');
+        }
+
+        $name = $input->getArgument('name');
+
+        $directory = $name && $name !== '.' ? getcwd() . '/' . $name : getcwd();
+
+        if (!$input->getOption('force')) {
+            $this->verifyApplicationDoesntExist($directory);
+        }
+
+        $output->writeln('<info>Construyendo aplicación...</info>');
+
+        $this->download($zipFile = $this->makeFilename(), $this->getVersion($input))
+            ->extract($zipFile, $directory)
+            ->cleanUp($zipFile);
+
+        $composer = $this->findComposer();
+
+        $commands = [
+            $composer . ' install',
+//            $composer . ' run-script post-root-package-install',
+//            $composer . ' run-script post-create-project-cmd',
+//            $composer . ' run-script post-autoload-dump',
+        ];
+
+        if ($input->getOption('no-ansi')) {
+            $commands = array_map(function ($value) {
+                return $value . ' --no-ansi';
+            }, $commands);
+        }
+
+        if ($input->getOption('quiet')) {
+            $commands = array_map(function ($value) {
+                return $value . ' --quiet';
+            }, $commands);
+        }
+
+        $process = Process::fromShellCommandline(implode(' && ', $commands), $directory, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $output->writeln('Warning: ' . $e->getMessage());
+            }
+        }
+
+        $process->run(function ($type, $line) use ($output) {
+            $output->write($line);
+        });
+
+        if ($process->isSuccessful()) {
+            $output->writeln('<comment>Aplicación preparada para empezar a crear.</comment>');
+        }
+
+        return 0;
+    }
+
+    /**
+     * Verify that the application does not already exist.
+     *
+     * @param string $directory
+     * @return void
+     */
+    protected function verifyApplicationDoesntExist($directory)
+    {
+        if ((is_dir($directory) || is_file($directory)) && $directory != getcwd()) {
+            throw new RuntimeException('La aplicación ya existe!');
+        }
+    }
+
+    /**
+     * Generate a random temporary filename.
+     *
+     * @return string
+     */
+    protected function makeFilename()
+    {
+        return getcwd() . '/grelo_' . md5(time() . uniqid()) . '.zip';
+    }
+
+    /**
+     * Download the temporary Zip to the given file.
+     *
+     * @param string $zipFile
+     * @param string $version
+     * @return $this
+     */
+    protected function download($zipFile, $version = 'master')
+    {
+        switch ($version) {
+            case 'develop':
+                $filename = 'latest-develop.zip';
+                break;
+            case 'master':
+                $filename = 'latest.zip';
+                break;
+        }
+
+        $response = (new Client)->get('http://artesanomultimedia.com/grelo/' . $filename);
+
+        file_put_contents($zipFile, $response->getBody());
+
+        return $this;
+    }
+
+    /**
+     * Extract the Zip file into the given directory.
+     *
+     * @param  string  $zipFile
+     * @param  string  $directory
+     * @return $this
+     */
+    protected function extract($zipFile, $directory)
+    {
+        $archive = new ZipArchive;
+
+        $response = $archive->open($zipFile, ZipArchive::CHECKCONS);
+
+        if ($response === ZipArchive::ER_NOZIP) {
+            throw new RuntimeException('No se pudo descargar el archivo. Compruebe que puede acceder a: http://artesanomultimedia.com/grelo/latest.zip');
+        }
+
+        $archive->extractTo($directory);
+
+        $archive->close();
+
+        return $this;
+    }
+
+    /**
+     * Clean-up the Zip file.
+     *
+     * @param  string  $zipFile
+     * @return $this
+     */
+    protected function cleanUp($zipFile)
+    {
+        @chmod($zipFile, 0777);
+
+        @unlink($zipFile);
+
+        return $this;
+    }
+
+    /**
+     * Get the version that should be downloaded.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @return string
+     */
+    protected function getVersion(InputInterface $input)
+    {
+        if ($input->getOption('dev')) {
+            return 'develop';
+        }
+
+        return 'master';
+    }
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    protected function findComposer()
+    {
+        $composerPath = getcwd().'/composer.phar';
+
+        if (file_exists($composerPath)) {
+            return '"'.PHP_BINARY.'" '.$composerPath;
+        }
+
+        return 'composer';
+    }
+
+}
